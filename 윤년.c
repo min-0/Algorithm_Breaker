9cc75e2490e65aada626413c8a9ea0733f77f860
min-0 <66067273+min-0@users.noreply.github.com>
2020-09-26 15:31:57 +0900

    2753번: 윤년
    연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오.
    
    윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
    
    예를 들어, 2012년은 4의 배수이면서 100의 배수가 아니라서 윤년이다. 1900년은 100의 배수이고 400의 배수는 아니기 때문에 윤년이 아니다. 하지만, 2000년은 400의 배수이기 때문에 윤년이다.

--- 윤년.c
+++ 윤년.c
@@ -4,17 +4,18 @@ int main()
 {
 	int year;
 
-	printf("년도를 입력하세요 : ");
-	scanf_s("%d", year);
+	scanf_s("%d", &year);
 
-	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+	if (year >= 1 && year <= 4000)
 	{
-		printf("윤년");
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}
 	}
-	else
-	{
-		printf("평년");
-	}
-
-
+	return 0;
 }
