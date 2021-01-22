c1006157dffece37816c5efa24779c72438844ab
min-0 <dhe08133@naver.com>
2021-01-22 10:58:18 +0900

    n찍기

---
+++ Java/n찍기.java
@@ -0,0 +1,21 @@
+// import java.util.*;
+
+// public class n찍기 {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// int n = sc.nextInt();
+
+// for (int i = 0; i < n; i++) {
+// for (int k = n - 1; k > i; k--) {
+// System.out.print(" ");
+// }
+// for (int k = 0; k <= i; k++) {
+// System.out.print("*");
+// }
+// System.out.print("\n");
+// }
+
+// sc.close();
+// }
+// }
