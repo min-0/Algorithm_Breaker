d9db4f440ac2ad8740bc6fb2b9da5d138bd7d4db
min-0 <dhe08133@naver.com>
2021-04-12 20:39:56 +0900

    1157번: 단어공부

--- C++/단어공부.cpp
+++ C++/단어공부.cpp
@@ -7,35 +7,35 @@ int main(){
 	string alpha;
 
 	int cnt = 0;
-	int max = 0;
-	int index[26] = { 0, };
+	int m = 0;
+	int index = 0;
+	int alphacnt[27] = { 0, };
 
 	cin >> alpha;
 
 	for (int i = 0; i <= alpha.length(); i++){
-		if (alpha[i] <= 65)
+		if (!(alpha[i] >= 'A' && alpha[i] <= 'Z') && !(alpha[i] >= 'a' && alpha[i] <= 'z')) continue; //알파벳만 받아오도록,,
+
+		if (alpha[i] >= 'A' && alpha[i] <= 'Z')
 		{
 			alpha[i] += 32;
 		}
+		int idx = alpha[i] - 'a';
+		alphacnt[idx]++;
+		m = alphacnt[idx] >= m ? alphacnt[idx] : m;
 	}
-	for (int i = 0; i < alpha.length(); i++){
-		for (int k = 0; k <= 26; k++){
-			if (alpha[i] == k + 97)
-				index[k]++;
-		}
-	}
-	for (int i = 0; i < 26; i++)
+	for (int i = 0; i < 27; i++)
 	{
-		if (index[i] > 1)
+		if (alphacnt[i] == m)
 		{
-			max = i;
 			cnt++;
+			index = i;
 		}
 	}
 
 	if (cnt > 1)
 		cout << "?" << endl;
 	else
-		cout << (char)(max + 65) << endl;
+		cout << (char)(index + 65) << endl;
 
 }
