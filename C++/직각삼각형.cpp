30c8cde096bacbb6867a23f09ab2d7fc174d4f67
min-0 <dhe08133@naver.com>
2021-05-03 18:56:08 +0900

    4153qjs: 직각삼각형

---
+++ C++/직각삼각형.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int heru, ausar, auset;
+	int sum = 0;
+
+	while (true){
+		cin >> heru >> ausar >> auset;
+
+		if (heru == 0 && ausar == 0 && auset == 0)
+		{
+			break;
+		}
+		else if (pow(heru, 2) + pow(ausar, 2) == pow(auset, 2)
+			|| pow(auset, 2) + pow(ausar, 2) == pow(heru, 2)
+			|| pow(heru, 2) + pow(auset, 2) == pow(ausar, 2))
+		{
+			cout << "right" << endl;
+		}
+		else cout << "wrong" << endl;
+	}
+}
