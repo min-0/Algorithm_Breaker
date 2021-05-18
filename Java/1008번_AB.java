6199886d44ec2c67e3dd079e44d8c1bf5e0d1835
min-0 <dhe08133@naver.com>
2021-05-18 23:29:00 +0900

    solved

---
+++ Java/1008번_AB.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class _1008번 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.print(a / b);
+
+        sc.close();
+    }
+}
