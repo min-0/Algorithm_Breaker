98174039499c7bda2ec245fa53b426e4cad1575b
정민영 <66067273+min-0@users.noreply.github.com>
2024-04-17 12:53:15 +0900

    [level 2] Title: H-Index, Time: 0.47 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        
+        for(int i = 0; i < citations.length; i++) {
+            int cnt = citations.length - i;
+            if(cnt <= citations[i]) {
+                answer = cnt;
+                break;
+            }
+        }
+        return answer;
+    }
+}
