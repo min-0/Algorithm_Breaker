7eed3d97c3f51300214b14b3d4c038f07d1b7f6a
정민영 <66067273+min-0@users.noreply.github.com>
2024-05-24 16:54:40 +0900

    [Gold V] Title: 강의실 배정, Time: 696 ms, Memory: 68520 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Time implements Comparable<Time> {
+        int start, end;
+
+        Time(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Time o) {
+            if (this.start == o.start) return this.end - o.end;
+            else return this.start - o.start;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        Time[] arr = new Time[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            arr[i] = new Time(s, e);
+        }
+
+        Arrays.sort(arr);  // 시작 시간 기준으로 정렬
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(arr[0].end);
+
+        for (int i = 1; i < n; i++) {
+            if (pq.peek() <= arr[i].start) pq.poll();
+            pq.add(arr[i].end); //새로운 강의실 추가
+        }
+
+        System.out.println(pq.size());
+    }
+}
