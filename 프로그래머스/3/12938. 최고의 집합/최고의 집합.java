298ac040f370065dca99479693ca3b268b7ed77a
정민영 <66067273+min-0@users.noreply.github.com>
2024-12-31 17:29:28 +0900

    [level 3] Title: 최고의 집합, Time: 1.50 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int s) {
+        if(n > s) return new int[] {-1};
+        int[] answer = new int[n];
+        Arrays.fill(answer, s / n);
+        
+        for(int i = 0; i < s % n; i++) {
+            answer[i] += 1;
+        }
+        
+        Arrays.sort(answer);
+        return answer;
+    }
+}
